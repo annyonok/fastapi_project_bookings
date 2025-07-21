ff531208f27ccff5136944f831d0874b77b2b636
annyonok <annyonok@gmail.com>
2025-07-21 13:12:42 +0300

    Fix Settings validation for Render deployment

--- app/config.py
+++ app/config.py
@@ -1,4 +1,4 @@
-from typing import Literal
+from typing import Literal, Optional
 
 from pydantic_settings import BaseSettings
 
@@ -7,21 +7,21 @@ class Settings(BaseSettings):
     MODE: Literal["DEV", "TEST", "PROD"]
     LOG_LEVEL: Literal["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]
 
-    DB_HOST: str
-    DB_PORT: int
-    DB_USER: str
-    DB_PASS: str
-    DB_NAME: str
+    DB_HOST: Optional[str] = None
+    DB_PORT: Optional[str] = None
+    DB_USER: Optional[str] = None
+    DB_PASS: Optional[str] = None
+    DB_NAME: Optional[str] = None
 
     @property
     def DATABASE_URL(self) -> str:
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
-    TEST_DB_HOST: str
-    TEST_DB_PORT: int
-    TEST_DB_USER: str
-    TEST_DB_PASS: str
-    TEST_DB_NAME: str
+    TEST_DB_HOST: Optional[str] = None
+    TEST_DB_PORT: Optional[str] = None
+    TEST_DB_USER: Optional[str] = None
+    TEST_DB_PASS: Optional[str] = None
+    TEST_DB_NAME: Optional[str] = None
 
     @property
     def TEST_DATABASE_URL(self) -> str:
